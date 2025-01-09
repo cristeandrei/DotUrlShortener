535042aad1323371ba4eb9ffd1aece44573363ff
acriste <cristeandreiflorin@gmail.com>
2025-01-09 18:09:26 +0200

    added aspire test to replace existing integration tests

---
+++ src/ReduceUrl.AppHost.Tests/ReduceUrlGetUrlsTests.cs
@@ -0,0 +1,46 @@
+namespace ReduceUrl.AppHost.Tests;
+
+[TestClass]
+public class ReduceUrlGetUrlsTests
+{
+    [TestMethod]
+    public async Task GetUrlsReturnsOkStatusCode()
+    {
+        // Arrange
+        var appHost =
+            await DistributedApplicationTestingBuilder.CreateAsync<Projects.ReduceUrl_AppHost>();
+
+        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
+        {
+            clientBuilder.AddStandardResilienceHandler();
+        });
+
+        await using var app = await appHost.BuildAsync();
+
+        var resourceNotificationService =
+            app.Services.GetRequiredService<ResourceNotificationService>();
+
+        await app.StartAsync();
+
+        // Act
+        using var httpClient = app.CreateHttpClient("reduceurlui");
+
+        await resourceNotificationService
+            .WaitForResourceAsync(
+                "reduceurldataaspiremigration",
+                KnownResourceStates.TerminalStates
+            )
+            .WaitAsync(TimeSpan.FromSeconds(30));
+
+        await resourceNotificationService
+            .WaitForResourceAsync("reduceurlui", KnownResourceStates.Running)
+            .WaitAsync(TimeSpan.FromSeconds(30));
+
+        var requestUri = new Uri("/Urls", UriKind.Relative);
+
+        var response = await httpClient.GetAsync(requestUri);
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+}
