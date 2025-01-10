22171069424f1e2c5cd24379b8f89e567ac3be43
acriste <cristeandreiflorin@gmail.com>
2025-01-10 17:20:35 +0000

    install aspire during project setup

--- .devcontainer/devcontainer-post-create-setup.sh
+++ .devcontainer/devcontainer-post-create-setup.sh
@@ -3,3 +3,7 @@ dotnet tool install --global Microsoft.OpenApi.Kiota
 dotnet tool install --global csharpier
 
 dotnet dev-certs https --trust
+
+dotnet workload update
+
+dotnet workload install aspire
